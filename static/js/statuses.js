78e433589eabb60a9cb77c8c1ab6b0312be2f196
Tor Johnson <kasigi@gmail.com>
2016-04-10 23:11:51 -0400

    Added error message to http operation

--- static/js/statuses.js
+++ static/js/statuses.js
@@ -111,7 +111,7 @@ jQuery(document).ready(function(){
         processMonitor(0);
     }
 
-if(document.location.protocol !== "https:"){
+if(document.location.protocol !== "http:"){
     var errorMsg = "<div class=\"alert alert-danger\" role=\"alert\">Warning: Parts of the CitySDK test suite REQUIRE non-SSL communication. View this page with http not https.</div>";
     jQuery("#citysdk-test-suite h2").after(errorMsg);
 }
