d16c1596868e661dfadeb6e237a0fc392ae91a96
Tejen Shrestha <shrestha_tejen@bah.com>
2016-06-01 15:50:41 -0400

    Removed hardcoded api key.

--- node-api/src/api/census.router.js
+++ node-api/src/api/census.router.js
@@ -3,22 +3,32 @@ var keyauth = require('./auth.service');
 var CensusModule = require('../../build/modules/census/citysdk.census');
 
 var router = express.Router();
-var census = new CensusModule.default('88c69cd2d93fae30723c3ec3546d66521f339255');
+
+var moduleInstantiated = false;
+var census;
+
+function getModuleInstance(req) {
+  if (!moduleInstantiated) {
+    var authHeader = req.header("Authorization").split(" ");
+    var apikey = authHeader[1];
+
+    census = new CensusModule.default(apikey);
+    return census;
+  }
+
+  return census;
+}
 
 router.post('/geo', keyauth, function(req, res) {
-  census.geoRequest(req.body, handleResponse);
-
-  function handleResponse(response) {
+  getModuleInstance(req).geoRequest(req.body, function(response) {
     res.json(response);
-  }
+  });
 });
 
 router.post('/api', keyauth, function(req, res) {
-  census.apiRequest(req.body, handler);
-
-  function handler(response) {
+  getModuleInstance(req).apiRequest(req.body, function(response) {
     res.json(response);
-  }
+  });
 });
 
 module.exports = router;
