25153f24d8afd5dafe1ba2537109188b988e069f
Tejen Shrestha <shrestha_tejen@bah.com>
2016-06-09 16:03:11 -0400

    Integrated Karma and Jasmine testing tools
    - Rewrote census module test-suite using Jasmine.

--- tests/test-suite.citysdk.arcgis.js
+++ tests/test-suite.citysdk.arcgis.js
@@ -69,6 +69,6 @@ function testArcGISModule() {
 
   updateStatusDisplay();
 
-}// testCensusModule
+}// censusModuleTest
 
 
--- tests/test-suite.citysdk.farmersMarket.js
+++ tests/test-suite.citysdk.farmersMarket.js
@@ -40,6 +40,6 @@ function testfarmersMarketModule() {
 
   updateStatusDisplay();
 
-}// testCensusModule
+}// censusModuleTest
 
 
