a5a54dcc8fd172cb2491bc58e3841bc27a06b870
Logan Powell <loganp@tepper.cmu.edu>
2018-11-07 07:39:29 -0500

    removed test release

--- dis/cljs-runtime/shadow.js.shim.module$dotenv.js
+++
@@ -1,4 +0,0 @@
-goog.provide("shadow.js.shim.module$dotenv");
-goog.provide("module$shadow_js_shim_module$dotenv");
-shadow.js.shim.module$dotenv = require("dotenv");
-module$shadow_js_shim_module$dotenv.default = shadow.js.shim.module$dotenv;
--- dis/cljs-runtime/shadow.umd_helper.js
+++
@@ -1,8 +0,0 @@
-goog.provide('shadow.umd_helper');
-goog.require('cljs.core');
-goog.require('workers.core');
-shadow.umd_helper.get_exports = (function shadow$umd_helper$get_exports(){
-var obj15004 = {"supercalifragilistic":workers.core.supercalifragilistic};
-return obj15004;
-});
-goog.exportSymbol('shadow.umd_helper.get_exports', shadow.umd_helper.get_exports);
