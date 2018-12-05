821d6adab0df925cba2f5fba5dc5f7bdfe6bd4d1
Logan Powell <loganp@tepper.cmu.edu>
2018-12-05 09:49:53 -0500

    changes `fill-opacity`

--- src/index.js
+++ src/index.js
@@ -4,35 +4,24 @@ let MapboxGeocoder = require("@mapbox/mapbox-gl-geocoder");
 let chroma = require("chroma-js");
 let _ = require("lodash");
 
-console.log("hello world");
 
 // === CENSUS PREP === //
-let isJsonString = function(str) {
-  try {
-    JSON.parse(str);
-  } catch (e) {
-    return false;
-  }
-  return true;
-};
-
 let logger = function(json) {
   document.getElementById("console").innerHTML = `${JSON.stringify(json,null,2)}`;
 };
 
 let setAndLog = function(res) {
-  if (isJsonString(res)) {
+  try {
     let json = JSON.parse(res);
     logger(json);
     return json;
-  } else {
+  } catch (e) {
     logger(res);
     return {};
   }
 };
 
 let censusPromise = function(args) {
-  console.log("In censusPromise");
   return new Promise(function(resolve, reject) {
     census(args, function(err, json) {
       if (!err) {
@@ -50,7 +39,6 @@ let values = ["B00001_001E"];
 let valueSelection = 0;
 let selection = values[valueSelection];
 let zoom = 9.0;
-let pitch = 45;
 
 // === CENSUS ARGUMENTS === //
 let Args = {
@@ -66,110 +54,48 @@ let Args = {
 
 // === TUNE CHOROPLETH VALUES  === //
 let quantiles = 5;
-let colorScale = chroma.scale(["#ffffff", "#000000"]).domain([0, 1]);
+// let colorScale = chroma.scale(["#ffffff", "#000000"]).domain([0, 1]);
+// let colorScale = chroma.scale('OrRd').domain([0, 1]);
+let colorScale = chroma.scale('PuBu').domain([0, 1]);
 
-censusPromise(Args).then(function(res) {
-  console.log(res);
-});
-// census(
-//   {
-//     vintage: "2015",
-//     geoHierarchy: {
-//       // state: {
-//       //   lat: 28.2639,
-//       //   lng: -80.7214
-//       // },
-//       county: "*"
-//     },
-//     sourcePath: ["acs", "acs5"],
-//     values: ["B00001_001E"],
-//     geoResolution: "500k"
-//   },
-//   (err, res) => console.log(res)
-// );
-
-// stats({
-//     vintage: 2016,
-//     sourcePath: ["acs", "acs5"],
-//     values: ['B01003_001E'],
-//     geoResolution: "500k",
-//     geoHierarchy: { state: "44", county: "*" }
-//   },
-//   (err, res) => console.log(res))
-
-// stats({
-//     vintage: 2016,
-//     sourcePath: ["acs", "acs5"],
-//     values: ['B25001_001E'],
-//     geoResolution: "500k",
-//     geoHierarchy: { state: "44", county: "*" }
-//   },
-//   (err, res) => console.log(res))
-
-// census({
-//     vintage: 2016,
-//     sourcePath: ["acs", "acs5"],
-//     values: ['B25001_001E'],
-//     geoResolution: "500k",
-//     geoHierarchy: { state: "44", county: "*" }
-//   },
-//   (err, res) => console.log(res))
 
 // === MAPBOX FUNCTIONS === //
 
-mapboxgl.accessToken = "pk.eyJ1Ijoib3BlbmlkZW8iLCJhIjoiY2pnemR0dmwyMHVhdDJ2bGV1bnl6amJqaiJ9._G3sOFQoJZklpO9pscg1mw";
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoib3BlbmlkZW8iLCJhIjoiY2pnemR0dmwyMHVhdDJ2bGV1bnl6amJqaiJ9._G3sOFQoJZklpO9pscg1mw";
 
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/openideo/cj48m1z521vzo2rqws9kwesra",
   center,
   zoom,
-  pitch
+  // pitch: 60
 });
 let geocoder = new MapboxGeocoder({
   accessToken: mapboxgl.accessToken
 });
 
+
+
 map.addControl(geocoder, "top-left");
-// placeholder for color scale created in quantileMaker
+
 let scale = [];
 
 let quantileMaker = function(min, max) {
-  console.log("In quantileMaker");
   let diff = max - min;
   let bucket = diff / quantiles;
   let dataScale = Array.apply(null, { length: quantiles + 1 })
     .map(Number.prototype.valueOf, 0)
-    .map(
-      function(val, idx) {
-        return idx === 0 ? min : (this.acc += bucket);
-      },
-      { acc: min }
-    );
-  let normalScale = dataScale.map(function(val, idx) {
-    return idx === 0 ? Math.round((min + 1 / max) * 100) / 100 : val / max;
-  });
-  let chromaScale = normalScale.map(function(val, idx) {
-    return colorScale(val).hex();
-  });
+    .map(function(val, idx) { return idx === 0 ? min : (this.acc += bucket)},{ acc: min });
+  let normalScale = dataScale
+    .map(function(val, idx) { return idx === 0 ? Math.round((min + 1 / max) * 100) / 100 : val / max });
+  let chromaScale = normalScale.map(function(val) { return colorScale(val).hex() });
   scale = _.zip(dataScale, chromaScale);
 };
 
 let getCensusData = async function(args) {
-  console.log("In getCensusData");
-  // Add the feature source
   let censusGeoJSON = await censusPromise(args);
-
-  // pull out features for color scaling:
   let features = censusGeoJSON.features;
-  /* ES6 destructuring if so inclined:
-  let { properties: { [selection]: maxVal } } = _.maxBy(features, function (o) {
-    return o.properties[selection];
-  });
-  let { properties: { [selection]: minVal } } = _.minBy(features, function (o) {
-    return o.properties[selection];
-  });
-  */
   let maxStat = _.maxBy(features, function(o) {
     return o.properties[selection];
   });
@@ -180,25 +106,9 @@ let getCensusData = async function(args) {
   let minVal = minStat.properties[selection];
   quantileMaker(minVal, maxVal);
   return { data: censusGeoJSON, stops: scale };
-  // map.addSource("census", {
-  //   type: "geojson",
-  //   data: censusGeoJSON
-  // });
-  // map.addLayer({
-  //   id: "stats",
-  //   type: "fill",
-  //   source: "census",
-  //   paint: {
-  //     "fill-color": {
-  //       property: selection,
-  //       stops: scale
-  //     },
-  //     "fill-outline-color": "#ffffff"
-  //   }
-  // });
 };
 
-// Random ID maker for each rendering to be unique
+// Random ID maker for each mapbox geocoder-rendered data view to be unique
 let makeid = function() {
   console.log("In makeID");
   let text = "";
@@ -210,12 +120,9 @@ let makeid = function() {
 };
 
 map.on("style.load", function() {
-  console.log("after style.load");
   geocoder.on("result", function(ev) {
-    // console.log(ev.result.geometry.coordinates);
     let sourceUID = makeid();
     let layerUID = makeid();
-
     let point = {
       lng: ev.result.geometry.coordinates[0],
       lat: ev.result.geometry.coordinates[1]
@@ -236,54 +143,14 @@ map.on("style.load", function() {
             property: selection,
             stops: res.stops
           },
-          "fill-outline-color": "#ffffff"
+          "fill-outline-color": "#ffffff",
+          "fill-opacity": 0.5
         }
       });
-    });
+    })
   });
 });
 
-//   async function() {
-//   // Add the feature source
-//   let response = await censusPromise(args);
-//   let censusGeoJSON = await response;
-
-//   // pull out features for color scaling:
-//   let features = censusGeoJSON.features;
-/* ES6 destructuring if so inclined:
-  let { properties: { [selection]: maxVal } } = _.maxBy(features, function (o) {
-    return o.properties[selection];
-  });
-  let { properties: { [selection]: minVal } } = _.minBy(features, function (o) {
-    return o.properties[selection];
-  });
-  */
-//   let maxStat = _.maxBy(features, function(o) {
-//     return o.properties[selection];
-//   });
-//   let maxVal = maxStat.properties[selection];
-//   let minStat = _.minBy(features, function(o) {
-//     return o.properties[selection];
-//   });
-//   let minVal = minStat.properties[selection];
-//   quantileMaker(maxVal, minVal);
-
-//   map.addSource("census", {
-//     type: "geojson",
-//     data: censusGeoJSON
-//   });
-//   map.addLayer({
-//     id: "stats",
-//     type: "fill",
-//     source: "census",
-//     paint: {
-//       "fill-color": {
-//         property: selection,
-//         stops: scale
-//       },
-//       "fill-outline-color": "#ffffff"
-//     }
-//   });
 
 // TODO: legend: https://www.mapbox.com/help/choropleth-studio-gl-pt-2/
 // TODO: https://www.mapbox.com/mapbox-gl-js/example/updating-choropleth/
