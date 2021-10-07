66d6fa8535cd81c8ef4f5256f3a23c41ccb16f89
Logan Powell <loganp@tepper.cmu.edu>
2021-10-07 14:09:15 -0400

    updates and hk

--- public/census/test/index.js
+++ public/census/test/index.js
@@ -1,4 +1,4 @@
-const census = require("../census.js")
+const census = require("../census.js");
 
 // census(
 //   {
@@ -28,32 +28,32 @@ const census = require("../census.js")
 //    (err, res) => console.log(JSON.stringify(res)),
 //) //?
 
-census(
-    {
-        vintage      : 2019,
-        geoHierarchy : {
-            county : { lat: 42.3601, lng: -71.0589 },
-            //"county subdivision" : "07000",
-        },
-        sourcePath   : [ "acs", "acs5" ],
-        values       : [ "group(B15003)" ],
-    },
-    (err, res) => console.log(JSON.stringify(res)),
-) //?
+//census(
+//    {
+//        vintage      : 2019,
+//        geoHierarchy : {
+//            county : { lat: 42.3601, lng: -71.0589 },
+//            //"county subdivision" : "07000",
+//        },
+//        sourcePath   : [ "acs", "acs5" ],
+//        values       : [ "group(B15003)" ],
+//    },
+//    (err, res) => console.log(JSON.stringify(res)),
+//) //?
 
 census(
-    {
-        vintage       : "2017",
-        geoHierarchy  : {
-            state  : {
-                lat : 38.8482,
-                lng : -76.932,
-            },
-            county : "*",
-        },
-        sourcePath    : [ "acs", "acs5" ],
-        values        : [ "B00001_001E" ],
-        geoResolution : "20m",
+  {
+    vintage: "2018",
+    geoHierarchy: {
+      state: {
+        lat: 38.8482,
+        lng: -76.932,
+      },
+      county: "*",
     },
-    (err, res) => console.log(res),
-)
+    sourcePath: ["acs", "acs5"],
+    values: ["B00001_001E"],
+    geoResolution: "500k",
+  },
+  (err, res) => console.log(res)
+); //?
