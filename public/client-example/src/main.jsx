c7aadf126d6fb4050d02235f356120c9ad0a766f
loganpowell <loganp@tepper.cmu.edu>
2022-12-06 08:24:09 -0500

    attempts to fix vite build issue

--- public/client-example/src/main.jsx
+++ public/client-example/src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 )
