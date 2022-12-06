b33ecb805d63503672e10947d34dac9fe5ead86d
loganpowell <loganp@tepper.cmu.edu>
2022-12-06 16:36:34 -0500

    clearing gh-pages branch

--- public/client-example/vite.config.js
+++ public/client-example/vite.config.js
@@ -6,6 +6,7 @@ import notifier from 'vite-plugin-notifier'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '',
     plugins: [react(), notifier()],
     server: {
         port: 4000,
