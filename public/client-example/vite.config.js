05caee292a281a26ace882f055cb917dd4d3ec0a
loganpowell <loganp@tepper.cmu.edu>
2022-12-06 16:20:03 -0500

    clearing gh-pages branch

--- public/client-example/vite.config.js
+++ public/client-example/vite.config.js
@@ -1,32 +1,72 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import inject from '@rollup/plugin-inject'
+import GlobalsPolyfills from '@esbuild-plugins/node-globals-polyfill'
+import notifier from 'vite-plugin-notifier'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '',
-    define: {
-        //global: 'globalThis',
-        //'process.version': null,
-        process: null,
+    plugins: [react(), notifier()],
+    server: {
+        port: 4000,
     },
     resolve: {
         alias: {
-            //process: 'process/browser',
+            /** browserify for @jbrowse/react-linear-genome-view */
             stream: 'stream-browserify',
-            //zlib: 'browserify-zlib',
-            //util: 'util',
         },
     },
-    plugins: [react()],
+    optimizeDeps: {
+        esbuildOptions: {
+            // Node.js global to browser globalThis
+            define: {
+                global: 'globalThis',
+            },
+            // Enable esbuild polyfill plugins
+            plugins: [
+                GlobalsPolyfills({
+                    process: true,
+                    buffer: true,
+                }),
+            ],
+        },
+    },
     build: {
         rollupOptions: {
-            //plugins: [inject({ Buffer: ['Buffer', 'Buffer'] })],
-            output: {
-                manualChunks: {
-                    citysdk: ['citysdk'],
-                },
-            },
+            plugins: [
+                GlobalsPolyfills({
+                    process: true,
+                    buffer: true,
+                }),
+            ],
         },
     },
 })
+
+//// https://vitejs.dev/config/
+//export default defineConfig({
+//    base: '',
+//    define: {
+//        global: 'globalThis',
+//        process: JSON.stringify(process),
+//    },
+//    resolve: {
+//        alias: {
+//            //process: 'process/browser',
+//            stream: 'stream-browserify',
+//            //zlib: 'browserify-zlib',
+//            //util: 'util',
+//        },
+//    },
+//    plugins: [react()],
+//    build: {
+//        rollupOptions: {
+//            plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
+//            //output: {
+//            //    manualChunks: {
+//            //        citysdk: ['citysdk'],
+//            //    },
+//            //},
+//        },
+//    },
+//})
