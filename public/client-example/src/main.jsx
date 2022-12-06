151b0b3474e3f40725ae7c708347d6d909a7cfd0
loganpowell <loganp@tepper.cmu.edu>
2022-12-06 09:05:36 -0500

    updates to new React DOM API

--- public/client-example/src/main.jsx
+++ public/client-example/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
+    //<React.StrictMode>
+    <App />
+    //</React.StrictMode>,
 )
